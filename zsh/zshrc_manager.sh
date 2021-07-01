3af94ae0d632082ed367d068526423c87445aca5
Mark Dube <mjdube99@gmail.com>
2021-07-01 17:42:34 -0500

    changed to not automatically start tmux everytime zsh is opened

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,10 +1,10 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 # Run tmux if exists
-if command -v tmux>/dev/null; then
-	[ -z $TMUX ] && exec tmux
-else 
-	echo "tmux not installed. Run ./deploy to configure dependencies"
-fi
+# if command -v tmux>/dev/null; then
+# 	[ -z $TMUX ] && exec tmux
+# else 
+# 	echo "tmux not installed. Run ./deploy to configure dependencies"
+# fi
 
 source ~/dotfiles/zsh/zshrc.sh
