4ba9db1d8c8f1c76198b6c127b2a18bfeb12c809
Mark Dube <mjdube99@gmail.com>
2021-08-20 18:54:33 -0500

    made multi-line for easier readability

--- i3/scripts/gaming.sh
+++ i3/scripts/gaming.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Docking for Gaming"
 # sets monitor stuff up for main monitor
-xrandr --output eDP-1 --auto --output DP-1-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-0 --off --output DP-1-4 --off
+xrandr --output eDP-1 --auto --output DP-1-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal \
+    --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-0 --off --output DP-1-4 --off
 sleep 0.5
 # launches polybar config to get bar for every monitor
 bash ~/dotfiles/polybar/launch.sh &
