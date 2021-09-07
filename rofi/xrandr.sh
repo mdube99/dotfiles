1db2057f2f2e59de6b4213c368d514f1e5655391
Mark Dube <mjdube99@gmail.com>
2021-09-07 00:22:17 -0500

    rofi for displays

---
+++ rofi/xrandr.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# options to be displyed
+option0="activate all monitors"
+option1="monitor eDP-1 off"
+option2="monitor DP-1-0 off"
+option3="monitor eDP-1 primary"
+option4="monitor DP-1-0 primary"
+option5="monitor DP-1-0 same as eDP-1"
+option6="monitor DP-1-0 right of eDP-1"
+option7="monitor DP-1-0 left of eDP-1"
+option8="monitor DP-1-0 above eDP-1"
+option9="gaming"
+option10="dock monitor"
+option11="jpl"
+
+# Variable passed to rofi
+options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7\n$option8\n$option9\n$option10\n$option11"
+
+selected="$(echo -e "$options" | rofi -lines 13 -dmenu -theme ~/dotfiles/rofi/rofi_config.rasi -p "xrandr")"
+
+case $selected in
+    $option0)
+        xrandr --auto;;
+    $option1)
+        xrandr --output eDP-1 --off;;
+    $option2)
+        xrandr --output DP-1-0 --off;;
+	$option3)
+		xrandr --output eDP-1 --primary;;
+	$option4)
+		xrandr --output DP-1-0 --primary;;
+    $option5)
+        xrandr --output DP-1-0 --same-as eDP-1;;
+    $option6)
+        xrandr --output DP-1-0 --right-of eDP-1;;
+    $option7)
+        xrandr --output DP-1-0 --left-of eDP-1;;
+    $option8)
+        xrandr --output DP-1-0 --above eDP-1;;
+    $option9)
+        bash $HOME/dotfiles/i3/scripts/gaming.sh;;
+    $option10)
+        bash $HOME/development/scripts/dock;;
+    $option11)
+        bash $HOME/dotfiles/i3/scripts/jpl;;
+esac
