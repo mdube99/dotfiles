f5e3e161922d4d2838c7aa2e85007143ce000cdb
Mark Dube <mjdube99@gmail.com>
2021-10-03 17:05:52 -0500

    updated i3 scripts

--- i3/scripts/docked_non_portrait.sh
+++ i3/scripts/docked_non_portrait.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 4480x180 --rotate normal --output DP-1-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --mode 1920x1080 --pos 0x180 --rotate normal --output DP-1-3 --off --output HDMI-1-0 --off --output DP-1-4 --off
+bash ~/dotfiles/i3/scripts/docked_non_portrait_layout.sh
 sleep 0.2
 # launches polybar config to get bar for every monitor
 bash ~/dotfiles/polybar/launch.sh &
