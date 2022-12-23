27ee56da265e1e7541408639222b8cf5d702b86e
Mark Dube <mjdube99@gmail.com>
2022-12-23 09:51:03 -0600

    updated to use zap, cleaned up files

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,16 +1,23 @@
-## LIB: Base Configuration (Copied from OMZ)
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
 
-source $HOME/dotfiles/zsh/lib/history.zsh
-source $HOME/dotfiles/zsh/lib/completions.zsh
-source $HOME/dotfiles/zsh/lib/keybindings.zsh
-source $HOME/dotfiles/zsh/lib/vim.zsh
-
-## CONFIGURATION: My Configuration
+plug "zsh-users/zsh-autosuggestions"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "zsh-users/zsh-history-substring-search"
+plug "zap-zsh/fzf"
+plug "zap-zsh/atmachine-prompt"
+plug "esc/conda-zsh-completion"
+plug "zap-zsh/vim"
+plug "zap-zsh/supercharge"
 
 source $HOME/dotfiles/zsh/conf/aliases.zsh
 source $HOME/dotfiles/zsh/conf/exports.zsh
 source $HOME/dotfiles/zsh/conf/theme.zsh
-source $HOME/dotfiles/zsh/conf/plugins.zsh
-source $HOME/dotfiles/zsh/conf/general.zsh
-source ~/dotfiles/zsh/conf/prompt.sh
 
+unsetopt BEEP
+
+# allows colors in zsh prompt
+autoload -U colors && colors
+setopt prompt_subst
+
+# autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#575757"
