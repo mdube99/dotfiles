cd80f050faf84b387833aacfad133fde79613592
Mark Dube <mjdube99@gmail.com>
2023-03-18 12:52:50 -0500

    removed unused functions/aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,37 +2,6 @@
 
 function tn() { tmux new-session -A -s $(basename "$PWD") }
 
-# Edit and rerun
-function edit_and_run() {
-    BUFFER="fc"
-    zle accept-line
-}
-zle -N edit_and_run
-bindkey "^v" edit_and_run
-
-# Enter
-function enter_line() {
-    zle accept-line
-}
-zle -N enter_line
-bindkey "^o" enter_line
-
-# Exit tmux-pane
-function exit_tmux() {
-    BUFFER="exit"
-    zle accept-line
-}
-zle -N exit_tmux
-bindkey "^q" exit_tmux
-
-# Sudo
-function add_sudo() {
-    BUFFER="sudo "$BUFFER
-    zle end-of-line
-}
-zle -N add_sudo
-bindkey "^s" add_sudo
-
 # Run last command again
 function runLastLine() {
     # Get previous history item
@@ -43,21 +12,8 @@ function runLastLine() {
 zle -N runLastLine
 bindkey "^n" runLastLine
 
-# Home - Navigates to the current root workspace
-function git_root() {
-    BUFFER="cd $(git rev-parse --show-toplevel || echo ".")"
-    zle accept-line
-}
-zle -N git_root
-bindkey "^g" git_root
-
 # Backwards search with C-r
 bindkey '^R' history-incremental-search-backward
-
-# I can never remember the exact command to convert from docx to md so i thought this would be easier
-convert2md() {
-    pandoc -s $1 --wrap=none --reference-links -t markdown -o $2
-}
 
 # easier to type to run docx or excel files from command line
 office() {
