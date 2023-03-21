d8a549ddbe73d6e75a35d8f0d87738deaee33a0c
Mark Dube <mjdube99@gmail.com>
2023-03-21 14:07:42 -0500

    added fzf stuff for bat

--- zsh/.config/zsh/zshrc.sh
+++ zsh/.config/zsh/zshrc.sh
@@ -24,3 +24,20 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
+#
+# use `bat` for preview if installed, `head` otherwise
+# https://github.com/sharkdp/bat
+if command -v bat > /dev/null 2>&1; then
+  FZF_PREVIEW_CMD="bat --style=numbers --color=always {} || ls -la {}"
+  # bat options
+  export BAT_PAGER="less -R"
+  export BAT_THEME="1337"
+else
+  FZF_PREVIEW_CMD="head -n FZF_PREVIEW_LINES {} || ls -la {}"
+fi
+# https://github.com/BurntSushi/ripgrep
+if command -v rg > /dev/null 2>&1; then
+  RG_OPTS="--files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null"
+  export FZF_DEFAULT_COMMAND="rg $RG_OPTS"
+  export FZF_CTRL_T_COMMAND="rg $RG_OPTS"
+fi
