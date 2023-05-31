8866e6082b0a43a67b48d5d7fd73d71c0ebc67f7
Mark Dube <mjdube99@gmail.com>
2023-05-30 22:59:59 -0500

    changed back to tokyodark because i cant decide

--- nvim/.config/nvim/lua/mark/plugins/init.lua
+++ nvim/.config/nvim/lua/mark/plugins/init.lua
@@ -99,8 +99,14 @@ return {
     {
         "sainnhe/gruvbox-material",
         priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme "gruvbox-material"
+        -- end,
+    },
+    {
+        "tiagovla/tokyodark.nvim",
         config = function()
-            vim.cmd.colorscheme "gruvbox-material"
+            vim.cmd.colorscheme "tokyodark"
         end,
     },
     {
