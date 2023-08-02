4cc85e2162e9fe324dc95e3b98134c1e2fe0d199
Mark Dube <mjdube99@gmail.com>
2023-08-02 15:57:54 -0500

    updated wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -38,4 +38,5 @@ return {
 	-- Settings
 	window_close_confirmation = "NeverPrompt", -- by default, will ask if you're sure you wanna quit.
 	show_update_window = false,
+	front_end = "Software",
 }
