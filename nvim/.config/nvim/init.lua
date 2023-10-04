c087d7457f57579f89f59cc0c5c82c4ffadd8a7e
Mark Dube <mjdube99@gmail.com>
2023-10-04 02:01:36 -0500

    added binding to check whether vim is within a git repo or not. will run find_files or git_files accordingly

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -19,7 +19,20 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+-- Will check if vim was started within git. changes telescope accordingly
+local utils = require('telescope.utils')
+local builtin = require('telescope.builtin')
+_G.project_files = function()
+    local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
+    if ret == 0 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end
+--
+-- vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>ff', '<cmd>lua project_files()<CR>', { desc = '[F]ind [F]iles' })
 vim.keymap.set('n', '<leader>fc', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
