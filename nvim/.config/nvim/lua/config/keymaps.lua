5dfd00ddfe8a2efbf1b5833b0d0ffe6fa6edaa66
Mark Dube <mjdube99@gmial.com>
2023-10-20 14:35:22 -0500

    use proper keymap file with lazy

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+vim.keymap.del('n', '<S-h>')
+vim.keymap.del('n', '<S-l>')
+
+vim.keymap.set('n', 'z.', ':<C-u>normal! zszH<CR>', { silent = true })
+
+function QuickFixToggle()
+  if vim.tbl_isempty(vim.fn.filter(vim.fn.getwininfo(), function(_, wininfo)
+    return wininfo.quickfix
+  end)) then
+    vim.cmd 'copen'
+  else
+    vim.cmd 'cclose'
+  end
+end
+
+vim.keymap.set('n', '<C-q>', ':lua QuickFixToggle()<CR>')
