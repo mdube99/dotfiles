dcf1ade6b93f2a74d90a9cfd4ed3382d0144be77
Mark Dube <mjdube99@gmial.com>
2023-10-20 14:35:43 -0500

    use proper with lazy

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,100 +1,57 @@
 return {
-    -- Git related plugins
-    'tpope/vim-fugitive',
-    -- Detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
-    {
-        'kylechui/nvim-surround',
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup()
-        end,
-    },
-    -- vscode snippets
-    { "rafamadriz/friendly-snippets",
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
-    -- NOTE: This is where your plugins related to LSP can be installed.
-    --  The configuration is done below. Search for lspconfig to find it below.
-    { -- LSP Configuration & Plugins
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-
-            -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            -- { 'j-hui/fidget.nvim', opts = {} },
-
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
-    },
-    {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end,
-    },
-    { -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-    },
-    -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim', opts = {} },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      opts = {
-        indent = {
-          char = "│",
-          tab_char = "│",
-        },
-        scope = { enabled = false },
-        exclude = {
-          filetypes = {
-            "help",
-            "alpha",
-            "dashboard",
-            "neo-tree",
-            "Trouble",
-            "lazy",
-            "mason",
-            "notify",
-            "toggleterm",
-            "lazyterm",
-          },
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  -- vscode snippets
+  {
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {
+      indent = {
+        char = '│',
+        tab_char = '│',
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
         },
       },
-      main = "ibl",
     },
-    -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
-    { -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
-        config = function()
-            pcall(require('nvim-treesitter.install').update { with_sync = true })
-        end,
-    },
-    { "LunarVim/darkplus.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "darkplus"
-        end,
-    },
-    -- {
-    --     "sainnhe/gruvbox-material",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.cmd.colorscheme "gruvbox-material"
-    --     end,
-    -- },
-    {
-        "lambdalisue/suda.vim",
-        cmd = "SudaWrite",
-    },
+    main = 'ibl',
+  },
+  {
+    'LunarVim/darkplus.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'darkplus'
+    end,
+  },
+  -- {
+  --     "sainnhe/gruvbox-material",
+  --     priority = 1000,
+  --     config = function()
+  --         vim.cmd.colorscheme "gruvbox-material"
+  --     end,
+  -- },
+  {
+    'lambdalisue/suda.vim',
+    cmd = 'SudaWrite',
+  },
+  { 'folke/flash.nvim', enabled = false },
 }
