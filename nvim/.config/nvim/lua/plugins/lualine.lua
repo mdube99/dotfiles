44fa9e183036a76ce2fa8eafbbd4f1e4f6e88df4
Mark Dube <mjdube99@gmial.com>
2023-10-21 14:54:23 -0500

    using lazyvims builtin lualine conf

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,89 +1,10 @@
-local function diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed
-    }
-  end
-end
-
 return {
-  "nvim-lualine/lualine.nvim",
-  enabled = true,
-  lazy = false,
-  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-  config = function()
-    require("lualine").setup {
-      options = {
-        theme = "auto",
-        icons_enabled = true,
-        section_separators = "",
-        component_separators = "",
-        disabled_filetypes = {
-          statusline = {
-            'help',
-            'startify',
-            'dashboard',
-            'neo-tree',
-            'packer',
-            'neogitstatus',
-            'NvimTree',
-            'Trouble',
-            'alpha',
-            'lir',
-            'Outline',
-            'spectre_panel',
-            'toggleterm',
-            'qf',
-          },
-          winbar = {},
-        },
-      },
-      sections = {
-        lualine_a = {"mode"},
-        lualine_b = {},
-        lualine_c = {
-          -- "filename",
-          {
-            "filetype",
-            icon_only = true,
-            separator = "",
-            padding = {
-              left = 1, right = 0 }
-          },
-          {
-            "filename",
-            path = 1,
-            symbols = {
-              modified = "  ",
-              readonly = "",
-              unnamed = ""
-            }
-          },
-          { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " " } },
-          { 'diff', source = diff_source },
-          { "searchcount" }
-        },
-        lualine_x = { { 'b:gitsigns_head', icon = '' } },
-        lualine_y = { "progress" },
-        lualine_z = {
-          -- function()
-          --   return " " .. os.date("%R")
-          -- end,
-        },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = { "neo-tree", "lazy" },
-    }
-  end,
+  -- the opts function can also be used to change the default opts:
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    opts = function(_, opts)
+      table.remove(opts.sections.lualine_b)
+    end,
+  },
 }
