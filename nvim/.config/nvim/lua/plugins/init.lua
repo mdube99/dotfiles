5013e5e83868cdef01c8e835b1fdc683ecc812b2
Mark Dube <mjdube99@gmail.com>
2024-04-17 11:30:08 -0500

    added colorschemes, added python extras

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -12,11 +12,27 @@ return {
   },
   {
     'LunarVim/darkplus.nvim',
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'darkplus'
+    -- end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'catppuccin-mocha'
+    -- end,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    name = 'kanagawa',
     config = function()
-      vim.cmd.colorscheme 'darkplus'
+      vim.cmd.colorscheme 'kanagawa-wave'
     end,
   },
+
   {
     'lambdalisue/suda.vim',
     cmd = 'SudaWrite',
